818cd5a098f2f43c67ec376511e16ab51aeb768b
AutiSahasra <sahasraauti7@gmail.com>
2026-07-31 01:16:33 +0530

    integrate dummy recipe API and log search results

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ let searchbtn=document.querySelector('#searchbtn')
 let inputfield=document.querySelector('#inputfield')
 let container=document.querySelector('.container')
 
+async function fetchRecipes(q){
+ const data=await fetch(`https://dummyjson.com/recipes/search?q=${q}`);
+ const res= await data.json();//consuming promise
+ console.log(res);
+}
+
+searchbtn.addEventListener('click',(e)=>
+{
+    e.preventDefault()//prevents reloading, when u click btn, performs logic
+    let query=inputfield.value.trim();
+    fetchRecipes(query)
+    console.log("button clicked")
+
+})
