dfbbf5f6910097d3f822ee5e30b72b05f46fafa0
AutiSahasra <sahasraauti7@gmail.com>
2026-07-31 19:02:35 +0530

    Displayed items on UI on search

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ async function fetchRecipes(q){
  const data=await fetch(`https://dummyjson.com/recipes/search?q=${q}`);
  const res= await data.json();//consuming promise
  console.log(res);
+ res.recipes.forEach(reciepe => {
+    let recipeDiv=document.createElement('div')
+    recipeDiv.classList.add('reciepeDiv')
+    
+    recipeDiv.innerHTML=`
+    <img src=${reciepe.image}>
+    <h2>Name: ${reciepe.name}</h2>
+    <h2>Cusine: ${reciepe.cuisine}</h2>
+    <h2>Calories per serving: ${reciepe.caloriesPerServing}</h2>
+    `
+    container.appendChild(recipeDiv)
+ });
+ 
 }
 
 searchbtn.addEventListener('click',(e)=>
